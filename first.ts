956fa08ee2ed6592f32fe7a3aa2f06e1d4b5e359
headsink <headsink@gmail.com>
2022-09-10 10:49:03 +0800

    just 3

---
+++ first.ts
@@ -0,0 +1,16 @@
+// first time using typescript -- this is a comment
+
+/* Multiline comment
+Don't forget to:
+
+# Install Locally in your project.
+npm install -D typescript
+npm install -D ts-node
+
+# Or globally with TypeScript.
+npm install -g typescript
+npm install -g ts-node
+
+# Depending on configuration, you may also need these
+npm install -D tslib @types/node
+*/
